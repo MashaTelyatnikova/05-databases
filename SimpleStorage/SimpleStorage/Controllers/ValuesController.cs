098c963cf83cf999787ceb8207ba05d87147a716
MashaTelyatnikova <mashatelatnikova@gmail.com>
2015-04-09 17:51:11 +0500

    Второе задание по шардингу.

--- SimpleStorage/SimpleStorage/Controllers/ValuesController.cs
+++ SimpleStorage/SimpleStorage/Controllers/ValuesController.cs
@@ -1,5 +1,8 @@
-﻿using System.Net;
+﻿using System;
+using System.Linq;
+using System.Net;
 using System.Web.Http;
+using Client;
 using Domain;
 using SimpleStorage.Infrastructure;
 
@@ -10,7 +13,7 @@ namespace SimpleStorage.Controllers
         private readonly IConfiguration configuration;
         private readonly IStateRepository stateRepository;
         private readonly IStorage storage;
-
+        private const string replica = "http://127.0.0.1:{0}/";
         public ValuesController(IStorage storage, IStateRepository stateRepository, IConfiguration configuration)
         {
             this.storage = storage;
@@ -27,6 +30,15 @@ namespace SimpleStorage.Controllers
         // GET api/values/5 
         public Value Get(string id)
         {
+            var port = GetPort(id);
+            if (port != configuration.CurrentNodePort)
+            {
+                var client = new SimpleStorageClient(string.Format(replica, port));
+                var res = client.Get(id);
+                if (res == null)
+                    throw new HttpResponseException(HttpStatusCode.NotFound);
+                return res;
+            }
             CheckState();
             var result = storage.Get(id);
             if (result == null)
@@ -37,8 +49,26 @@ namespace SimpleStorage.Controllers
         // PUT api/values/5
         public void Put(string id, [FromBody] Value value)
         {
-            CheckState();
-            storage.Set(id, value);
+            var port = GetPort(id);
+            if (port != configuration.CurrentNodePort)
+            {
+                var client = new SimpleStorageClient(string.Format(replica, port));
+                client.Put(id, value);
+            }
+            else
+            {
+                CheckState();
+                storage.Set(id, value);
+            }
+
+        }
+
+        private int GetPort(string id)
+        {
+            var ports = configuration.OtherShardsPorts.Concat(new[] { configuration.CurrentNodePort }).OrderBy(port => port).ToList();
+            var index = Math.Abs(id.GetHashCode()) % (configuration.OtherShardsPorts.Count() + 1);
+
+            return ports[index];
         }
     }
 }
